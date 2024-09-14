1cd196669868c8bc38a07062d91514bad13e7c51
juan jose <juan.jose.petit@hotmail.com>
2024-09-14 11:35:32 -0300

    config deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import robotsTxt from "astro-robots-txt"
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
   site: 'https://JuanJosePetit.github.io',
-  base: '/portafolio/',
+  base: '/',
 })
